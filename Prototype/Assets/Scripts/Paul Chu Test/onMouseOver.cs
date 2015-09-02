53f2987920c0aee32d4cefbbf2e486f744962816
Chui <lordbabypanda@gmail.com>
2015-09-02 18:09:51 -0500

    Hover
    Hover

---
+++ Prototype/Assets/Scripts/Paul Chu Test/onMouseOver.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class onMouseOver : MonoBehaviour, IPointerEnterHandler, IPointerEnterHandler
+{
+
+    private bool hover;
+ void Start()
+    {
+        hover = false; // for all cards to not hover in the beginning 
+    }
+}
