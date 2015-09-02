ffbd87042678258c26c10a5d3c6cac9fa3183b12
coringuyen <coringuyen96@gmail.com>
2015-09-02 18:10:19 -0500

    Update

---
+++ Prototype/Assets/Scripts/Clickable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clickable : MonoBehaviour {
+
+    public Camera cam;
+    public GameObject card1;
+ 
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            card1.transform.position = cam.ScreenToViewportPoint(transform.position);
+        }
+        
+	}
+}
