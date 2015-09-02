a20f80e7e026eb7311baa36bf87dc66057559b1b
Dylan Guidry <thebrokekiller@yahoo.com>
2015-09-02 18:08:36 -0500

    Initial Commit
    Getting the initial setup for a GUI

---
+++ Prototype/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void turnChange()
+    {
+        /*
+            calls a turn transition function from another class
+        */
+    }
+    public void gamePlay(string name)
+    {
+        Application.LoadLevel(name);
+        /*
+            Will use a function of the level loader class to load different scenes when 
+            a button is clicks
+        */
+    }
+
+    public void closeGAme()
+    {
+        Application.Quit();
+        /*
+            closes the application
+        */
+    }
+}
---
+++ Prototype/Assets/Scripts/GUIObjButtons.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIObjButtons : MonoBehaviour
+{
+    public int selGridInt = 0;
+    public string[] selStrings = new string[] { "Grid 1", "Grid 2", "Grid 3", "Grid 4" };
+    bool display = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    void OnGUI()
+    {
+        if(display == true)
+        GUI.SelectionGrid(new Rect(25, 25, 100, 30), selGridInt, selStrings, 2);
+    }
+
+    void OnMouseDown()
+    {
+        display = true;
+        print("ImHere");
+    }
+}
