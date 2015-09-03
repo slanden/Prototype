d47c5ebb9c49e2a582c243d5ff962228e801ceb4
slanden <slanden523@gmail.com>
2015-09-03 16:24:13 -0500

    -added table texture -added playerNetworkSetup script

---
+++ Prototype/Assets/Scripts/playerNetworkSetup.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class playerNetworkSetup : NetworkBehaviour
+{
+    [SyncVar]
+    private string playerUniqueIdentity;
+    private NetworkInstanceId playerNetID;
+    private Transform myTransform;
+
+    public override void OnStartLocalPlayer()
+    {
+        //base.OnStartLocalPlayer();
+        GetNetIdentity();
+        SetIdentity();
+    }
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<playerControls>().enabled = true;
+        }
+    }
+
+    void Update()
+    {
+        if (myTransform.name == "" || myTransform.name == "Player(Clone)")
+        {
+            SetIdentity();
+        }
+    }
+
+
+    [Client]
+    void GetNetIdentity()
+    {
+        playerNetID = GetComponent<NetworkIdentity>().netId;
+        CmdTellServerMyIdentity(MakeUniqueIdentity());
+    }
+
+    void SetIdentity()
+    {
+        if (isLocalPlayer)
+        {
+            myTransform.name = playerUniqueIdentity;
+        }
+        else
+        {
+            myTransform.name = MakeUniqueIdentity();
+        }
+    }
+
+    string MakeUniqueIdentity()
+    {
+        string uniqueIdentity = "Player" + playerNetID.ToString();
+        return uniqueIdentity;
+    }
+
+    [Command]
+    void CmdTellServerMyIdentity(string name)
+    {
+        playerUniqueIdentity = name;
+    }
+}
