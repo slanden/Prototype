6dbc83560026c18d23cec0bf5941b4545c55625c
lordbabypanda <lordbabypanda@gmail.com>
2015-09-03 08:39:11 -0500

    On it
    On it!

---
+++ Assets/Scripts/Paul Chu Test/onMouseOver.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems; // for point enter and exit
+
+public class onMouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private bool hover;
+
+    void Start()
+    {
+        hover = false; // for all cards to not hover in the beginning 
+    }
+
+    void Update()
+     {
+
+     }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        Debug.Log(gameObject);
+            if(hover == false)
+            {
+                transform.Translate(0,10,0);
+                hover = true;
+            }
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        transform.Translate(0, -10, 0);
+        hover = false;
+    }
+
+}
