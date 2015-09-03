0d4b8cca6bc32c746a245349eb25ed1021e086fe
Zac-King <Zacharyking18@yahoo.com>
2015-09-03 13:41:18 -0500

    Added FSM
    FSM Works

--- Prototype/Assets/Scripts/FSM_GamePlay.cs
+++ Prototype/Assets/Scripts/FSM_GamePlay.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FSM_GamePlay
+public class FSM_GamePlay : MonoBehaviour
 {
     /// <Game Play FSM>
     /// Control the flow  
@@ -22,48 +22,174 @@ public class FSM_GamePlay
         /// Check Fails if transisiton attempt is the same af current state
         if (currentState == to)
         {
+            print("GamePlay Transistion Failed.\nTransistion to current state is not valid.\nSee <FSM_GamePlay.cs>");
             return false;
         }
 
-        switch (to)
+        switch (currentState)
         {
-            case (FSM_GamePlay.States.STATE1):
-                {
+            case (States.STATE1):
                     /// PlaceHolder
-                     if(to == FSM_GamePlay.States.STATE2)
+                    print("GamePlay Transistion Check Passed.");
+                    if (to == States.STATE2)
                     return true;
-                }break;
-            case (FSM_GamePlay.States.STATE2):
-                {
+                break;
+
+            case (States.STATE2):
                     /// PlaceHolder
-                    if(to == FSM_GamePlay.States.STATE3)
+                    print("GamePlay Transistion Check Passed.");
+                    if (to == States.STATE3)
                     return true;
-                }break;
-            case (FSM_GamePlay.States.STATE3):
-                {
+                break;
+
+            case (States.STATE3):
                     /// PlaceHolder
-                    if (to == FSM_GamePlay.States.STATE4)
+                    print("GamePlay Transistion Check Passed.");
+                    if (to == States.STATE4)
                         return true;
-                }break;
-            case (FSM_GamePlay.States.STATE4):
-                {
+                break;
+
+            case (States.STATE4):
                     /// PlaceHolder
-                    if (to == FSM_GamePlay.States.STATE1)
+                    print("GamePlay Transistion Check Passed.");
+                    if (to == States.STATE1)
                         return true;
-                }break;
+                break;
+
             default:
                 break;
         }
         return false;
     }
 
-    bool HandleTransistion(States from, States to)
+    void HandleTransistion(States to)
     {
-        if (CheckTransistion(from, to))
+        if (CheckTransistion(to))
         {
+            switch(to)
+            {
+                case (States.STATE1):
+                    /// PlaceHolder
+                    PrintDebugText(currentState, to);
+                    currentState = States.STATE1;
+                    break;
 
+                case (States.STATE2):
+                    /// PlaceHolder
+                    PrintDebugText(currentState, to);
+                    currentState = States.STATE2;
+                    break;
+
+                case (States.STATE3):
+                    /// PlaceHolder
+                    PrintDebugText(currentState, to);
+                    currentState = States.STATE3;
+                    break;
+
+                case (States.STATE4):
+                    /// PlaceHolder
+                    PrintDebugText(currentState, to);
+                    currentState = States.STATE4;
+                    break;
+
+                default:
+                    print("GamePlay Transistion Failed.\nHandle Transistion returned False.\nSee < FSM_GamePlay.cs > ");
+                    break;
+            }
         }
-
-        return false;
     }
+
+    void PrintDebugText(States from, States to)
+    {
+        switch (from)
+        {
+            
+            case (FSM_GamePlay.States.STATE1):
+            {
+                switch(to)
+                    {
+                        case (FSM_GamePlay.States.STATE2):
+                            {
+                                print("Transistioning From <STATE1> to <STATE2>");
+                            }break;
+                        case (FSM_GamePlay.States.STATE3):
+                            {
+                                print("Transistioning From <STATE1> to <STATE3>");
+                            }
+                            break;
+                        case (FSM_GamePlay.States.STATE4):
+                            {
+                                print("Transistioning From <STATE1> to <STATE4>");
+                            }
+                            break;
+                    }
+            }break;
+
+            case (FSM_GamePlay.States.STATE2):
+                {
+                    switch (to)
+                    {
+                        case (FSM_GamePlay.States.STATE1):
+                            {
+                                print("Transistioning From <STATE2> to <STATE1>");
+                            }break;
+                        case (FSM_GamePlay.States.STATE3):
+                            {
+                                print("Transistioning From <STATE2> to <STATE3>");
+                            }break;
+                        case (FSM_GamePlay.States.STATE4):
+                            {
+                                print("Transistioning From <STATE2> to <STATE4>");
+                            }break;
+                    }
+                }break;
+
+            case (FSM_GamePlay.States.STATE3):
+                {
+                    switch (to)
+                    {
+                        case (FSM_GamePlay.States.STATE1):
+                            {
+                                print("Transistioning From <STATE3> to <STATE1>");
+                            }break;
+                        case (FSM_GamePlay.States.STATE2):
+                            {
+                                print("Transistioning From <STATE3> to <STATE2>");
+                            }break;
+                        case (FSM_GamePlay.States.STATE4):
+                            {
+                                print("Transistioning From <STATE3> to <STATE4>");
+                            }break;
+                    }
+                }break;
+            case (FSM_GamePlay.States.STATE4):
+                {
+                    switch (to)
+                    {
+                        case (FSM_GamePlay.States.STATE1):
+                            {
+                                print("Transistioning From <STATE4> to <STATE1>");
+                            }break;
+                        case (FSM_GamePlay.States.STATE2):
+                            {
+                                print("Transistioning From <STATE4> to <STATE2>");
+                            }break;
+                        case (FSM_GamePlay.States.STATE3):
+                            {
+                                print("Transistioning From <STATE4> to <STATE3>");
+                            }break;
+                    }
+                }break;
+
+            default:
+                break;
+        }
+    
+    }
+
+    //void Start()
+    //{
+    //    HandleTransistion(States.STATE2);
+    //}
+
 }
