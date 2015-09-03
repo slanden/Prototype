b1e7e55b71b18b3244a0b9b8fa951ef8a56ea64f
slanden <slanden523@gmail.com>
2015-09-03 11:57:25 -0500

    fixed movement
    fixed directions and speed

--- Prototype/Assets/Scripts/playerControls.cs
+++ Prototype/Assets/Scripts/playerControls.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class playerControls : MonoBehaviour {
 
     GameObject player;
+    float speed = 0.15f;
 
     // Use this for initialization
     void Start()
@@ -14,13 +15,13 @@ public class playerControls : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-            gameObject.GetComponent<Transform>().position -= new Vector3(1, 0, 0);
-        if (Input.GetKeyDown(KeyCode.D))
-            gameObject.GetComponent<Transform>().position += new Vector3(1, 0, 0);
-        if (Input.GetKeyDown(KeyCode.W))
-            gameObject.GetComponent<Transform>().position += new Vector3(0, 0, 1);
-        if (Input.GetKeyDown(KeyCode.S))
-            gameObject.GetComponent<Transform>().position -= new Vector3(0, 0, 1);
+        if (Input.GetKey(KeyCode.A))
+            gameObject.transform.position += Vector3.left * speed; //gameObject.GetComponent<Transform>().position -= new Vector3(1, 0, 0);
+        if (Input.GetKey(KeyCode.D))
+            gameObject.transform.position += Vector3.right * speed;
+        if (Input.GetKey(KeyCode.W))
+            gameObject.transform.position += Vector3.forward * speed; //new Vector3(0, 0, 1);
+        if (Input.GetKey(KeyCode.S))
+            gameObject.transform.position += Vector3.back * speed; //new Vector3(0, 0, 1);
     }
 }
